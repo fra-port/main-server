cc6b201f1b5acaab6284b0ff7a05e036eba48f18
sumarsanaadi <sumarsana.adi@gmail.com>
2018-10-17 21:14:32 +0700

    reenable get selling by id back

--- routes/selling.js
+++ routes/selling.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const { createSelling, updateSelling, getSelling, removeSelling, findSelling, findUserTodaySelling} = require('../controllers/sellingHistory');
 
 router.get('/', getSelling)
-      // .get('/:id', findSelling)
+      .get('/:id', findSelling)
       .get('/today/:id', findUserTodaySelling)
       .post('/', createSelling )
       .put('/:id', updateSelling)
